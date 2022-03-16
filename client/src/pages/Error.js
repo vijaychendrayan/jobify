cf79cb9da6a7098336511763fbebc65bbec059ca
Vijay Chendrayan Chandrasekar <vijaychendrayan@gmail.com>
2022-03-16 15:12:52 +0530

    s

--- client/src/pages/Error.js
+++ client/src/pages/Error.js
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom"
 import img from '../assets/images/not-found.svg'
 import Wrapper from "../assets/wrappers/ErrorPage"
+// test
 
 export const Error = () => {
   return (
