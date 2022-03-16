802ed415fb703c3bb338fc193bee12db8096280b
Vijay Chendrayan Chandrasekar <vijaychendrayan@gmail.com>
2022-03-16 20:10:21 +0530

    aa

---
+++ client/src/context/appContext.js
@@ -0,0 +1,29 @@
+import { useContext, useReducer, useContext } from "react"
+
+export const initialState ={
+    isLoading: false,
+    showAlert: false,
+    alertText: '',
+    alertType: '',
+}
+
+const AppContext = React.createContext()
+const AppProvider = ({ children }) =>{
+    const[state, setState] = useState(initialState)
+
+    return(
+        <AppContext.Provider
+            value={{
+                ...state
+            }}
+        >
+            {children}
+        </AppContext.Provider>
+    )
+}
+
+export const useAppContext = () => {
+    return useContext(AppContext)
+}
+
+export {AppProvider}
