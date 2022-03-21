34053655971028eb90902cffc23dfdca2037f00c
Vijay Chendrayan Chandrasekar <vijaychendrayan@gmail.com>
2022-03-21 10:30:37 +0530

    Nested Pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
-import {Dashboard, Landing, Register,  Error} from './pages'
+import { Landing, Register,  Error,ProtectedRoute} from './pages'
+import {AddJob, AllJobs, Profile, Stats, SharedLayout} from './pages/dashboard'
 
 
 function App() {
@@ -8,7 +9,15 @@ function App() {
 
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard/>} />
+        <Route path="/"  element={
+          <ProtectedRoute>
+            <SharedLayout />
+          </ProtectedRoute>}>
+          <Route index   element={<Stats />} />
+          <Route path="all-jobs" element={<AllJobs />} />
+          <Route path="add-job" element={<AddJob />} />
+          <Route path="profile" element={<Profile />} />
+        </Route>
         <Route path="/register" element={<Register/>} />
         <Route path="/landing" element={<Landing/>} />
         <Route path="*" element={<Error/>} />
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import Wrapper from '../assets/wrappers/Navbar'
+
+const Navbar = () => {
+    return(
+        <Wrapper>
+            <h4>Navagation Bar</h4>
+        </Wrapper>
+    )
+
+}
+
+export default Navbar
